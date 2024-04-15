f1b71931657de2dfe6520461127c2d29a1defc03
shumieb <shumiebc@gmail.com>
2024-04-15 22:14:56 +0100

    added 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Button from "./Button/Button"
+import Student from "./Student"
 
 function App() {
   return (
     <>
-      <Button/>
-    </>
-    
+      <Student name="My name" age={30} isStudent={true}/>
+      <Student name="My name2" age={32} isStudent={false}/>
+      <Student name="My name3" age={34} isStudent={true}/>
+      <Student name="My name4" age={35} isStudent={false}/>
+      <Student/>
+    </>    
   )
 }
 
